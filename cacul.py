0a3fee6531f51ff4a64c5f792318b0a23cd72553
Léonie Berton <leonie.berton.auditeur@lecnam.net>
2023-07-12 17:28:19 +0200

    new add

--- cacul.py
+++ cacul.py
@@ -5,4 +5,32 @@ from scipy import stats as st
  
 dataset = pd.read_csv("Test_Data.csv", sep=",")
 
-print (dataset.head())
+###### step 1: descriptive statistics  #####
+##print (dataset.head())
+
+time = dataset['T.T escena (ms)']
+
+print("Variable Time : ")
+## Mean
+mean = np.mean(time)
+print ("Mean : ", round(mean, 2))
+
+## Median
+median = np.median(time)
+print ("Median : ", round(median, 2))
+
+## Quartille
+
+Q1 = round(np.percentile(time, 25))
+print("Quartile Q1 : ", Q1)
+
+Q3 = round(np.percentile(time, 75))
+print("Quartile Q3 : ", Q3)
+
+## Variance
+variance = np.var(time)
+print ("Variance : ", round(variance, 2))
+
+## Standard deviation
+Stdeviation = np.std(time)
+print ("Standard deviation : ", round(Stdeviation, 2))
